5bf5959e6d316e0f4075fcdae605dcc292467c52
mchuta <mchuta@gmail.com>
2017-11-12 21:46:26 -0600

    Update student_test_types.sql

--- student_test_types.sql
+++ student_test_types.sql
@@ -3,3 +3,11 @@ CREATE TABLE [dbo].[student_test_types](
 	[name] [nvarchar](100) NOT NULL
 )
 GO
+
+INSERT INTO student_test_types (name) values ('SAT')
+INSERT INTO student_test_types (name) values ('ACT')
+INSERT INTO student_test_types (name) values ('GRE')
+INSERT INTO student_test_types (name) values ('PSAT')
+INSERT INTO student_test_types (name) values ('PSAT/NMSQT')
+INSERT INTO student_test_types (name) values ('LSAT')
+INSERT INTO student_test_types (name) values ('MCAT')
