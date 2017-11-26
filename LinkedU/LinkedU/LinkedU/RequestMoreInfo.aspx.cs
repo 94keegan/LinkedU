13e615b1222a5f4f7c2185733d70d1a8e4bde77b
zalliso <33206966+zalliso@users.noreply.github.com>
2017-11-26 15:59:29 -0600

    Add files via upload

---
+++ LinkedU/LinkedU/LinkedU/RequestMoreInfo.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Net.Mail;
+
+namespace LinkedU
+{
+    public partial class RequestMoreInfo : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void btnRequest_Click(object sender, EventArgs e)
+        {
+            String email = string.Format("{0}", Request.Form["email"]);
+            String customMessage = string.Format("{0}", Request.Form["message"]);
+
+            string connectionString = ConfigurationManager.ConnectionStrings["LinkedUConnectionString"].ConnectionString;
+            MailMessage msg = new MailMessage();
+            System.Net.Mail.SmtpClient client = new System.Net.Mail.SmtpClient();
+            using (SqlConnection dbConnection = new SqlConnection(connectionString))
+            {
+
+
+
+            }
+            try
+            {
+                msg.Subject = "Request for more information";
+                msg.Body = "User is interested in finding out more about you. They have requested that you send them more information about yourself. Please contact them at  " + email + " at your earliest convenience to find out more about what they have to offer. " + customMessage;
+                msg.From = new MailAddress("linkedu368@gmail.com");
+                msg.To.Add(email);
+                msg.IsBodyHtml = true;
+                client.Host = "smtp.gmail.com";
+                System.Net.NetworkCredential basicauthenticationinfo = new System.Net.NetworkCredential("linkedu368@gmail.com", "isuit368");
+                client.Port = int.Parse("587");
+                client.EnableSsl = true;
+                client.UseDefaultCredentials = false;
+                client.Credentials = basicauthenticationinfo;
+                client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                client.Send(msg);
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
