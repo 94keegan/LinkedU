316a36ea23fe85df5a5ff7f40f2e4dd655adc82a
zalliso <33206966+zalliso@users.noreply.github.com>
2017-11-26 15:54:11 -0600

    Add files via upload

---
+++ LinkedU/LinkedU/LinkedU/Default2.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Web.UI;
+
+namespace LinkedU
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                if (Request.Cookies["UserName"] == null)
+                {
+                    Console.WriteLine("Wish to register?");
+                }
+            }
+        }
+    }
+}
